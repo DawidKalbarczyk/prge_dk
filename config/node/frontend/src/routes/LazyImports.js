af304a1d797ed32f269b7e834225509c9ed49bf4
Dawid <u56128@tudent.wat.edu.pl>
2025-12-08 20:55:04 +0100

    feat: update comments and routes to lazy

--- config/node/frontend/src/routes/LazyImports.js
+++ config/node/frontend/src/routes/LazyImports.js
@@ -1,3 +1,7 @@
 import {lazy} from 'react';
 
 export const Home = lazy(() => import('../pages/Home.js'));
+export const About = lazy(() => import('../pages/About.js'));
+export const ListOfItems = lazy(() => import('../pages/ListOfItems.js'));
+export const Map = lazy(() => import('../pages/Map.js'));
+export const Services = lazy(() => import('../pages/Services.js'));
