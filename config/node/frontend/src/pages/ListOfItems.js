85570bb6bf8af82ae4e7bb3d6c8e0555f00cb000
Dawid <u56128@tudent.wat.edu.pl>
2026-01-12 14:13:14 +0100

    feat: added dynamic requests to backend

--- config/node/frontend/src/pages/ListOfItems.js
+++ config/node/frontend/src/pages/ListOfItems.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import UserCard from "../components/UserCard";
 
 function ListOfItems(props) {
+    //https://jsonplaceholder.typicode.com/users
+    // UseEffect nasłuchuje jakąś rzecz i w przypadku jej zmiany wywołuje kod
+    const [users, setUsers] = useState([]); //Tworzenie hooka, który może przechowywać coś w pamięci
+
+    useEffect(() => {
+        fetch('http://localhost:10000/app/get_users')
+            .then(res => res.json())
+            .then(res => {
+                console.log(res);
+                setUsers(res)
+            })
+        // Pobiera dane ze źródła
+    },[])
+
+
+
     return (
-        <div>ListOfItems</div>
+        <div>
+            ListOfItems
+            <div>
+                {users.data?.map(user => <UserCard user={user}/>)}
+            </div>
+        </div>
     );
 }
 
